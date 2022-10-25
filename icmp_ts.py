f79672904d4c206859b5a11899c5b50d56ac2230
s0i37 <s0i37@work>
2022-10-25 20:19:39 +0400

    added icmp_ts.py

--- icmp_ts.py
+++ icmp_ts.py
@@ -5,16 +5,32 @@ from scapy.all import sr1, IP, ICMP, conf
 from sys import argv
 import datetime
 import time
+from netaddr import IPNetwork
+from socket import ntohl
 
-TIMEOUT = 10
-target = argv[1]
+TIMEOUT = 2
+targets = argv[1]
 conf.verb = 0
 
-answer = sr1( IP(dst=target)/ICMP(type=13, code=0), timeout=TIMEOUT )
-answer[ICMP].ts_ori
-answer[ICMP].ts_rx
-answer[ICMP].ts_tx
-time_ori = (datetime.datetime(1,1,1)+datetime.timedelta(milliseconds=answer[ICMP].ts_ori)).strftime('%H:%M:%S')
-print("remote_time=%s, local_time=%s"%(time_ori, datetime.datetime.now().strftime('%H:%M:%S')))
+def get_delta_time(target):
+	answer = sr1( IP(dst=target)/ICMP(type=13, code=0), timeout=TIMEOUT )
+	if not answer or not ICMP in answer:
+		return
+	ts_ori = answer[ICMP].ts_ori
+	ts_rx = answer[ICMP].ts_rx
+	ts_tx = answer[ICMP].ts_tx
+	time_ori_fmt = (datetime.datetime(1,1,1)+datetime.timedelta(milliseconds=ts_ori)).strftime('%H:%M:%S')
+	if 128 < answer[IP].ttl <= 255:
+		ts_tx = ts_tx # Cisco unsupported
+	elif 64 < answer[IP].ttl <= 128:
+		ts_tx = ntohl(ts_tx)
+	elif answer[IP].ttl <= 64:
+		ts_tx = ts_tx
+	ts_tx_fmt = (datetime.datetime(1,1,1)+datetime.timedelta(milliseconds=ts_tx)).strftime('%H:%M:%S')
+	sign = "+" if ts_tx >= ts_ori else "-"
+	delta = (datetime.datetime(1,1,1)+datetime.timedelta(milliseconds=abs(ts_tx - ts_ori))).strftime('%H:%M:%S')
+	print("%s: local UTC=%s, remote UTC=%s, delta=%s%s (ttl=%d)"%(target, time_ori_fmt, ts_tx_fmt, sign, delta, answer[IP].ttl))
+	time.sleep(1)
 
-#https://www.rfc-editor.org/rfc/rfc792.html
+for ip in IPNetwork(targets):
+	get_delta_time(str(ip))
