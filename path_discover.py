d593bf934032c09a11455a5790748d0526808d07
s0i37 <s0i37@work>
2023-12-10 22:40:19 +0500

    bit changes in path_discover.py

--- path_discover.py
+++ path_discover.py
@@ -3,6 +3,7 @@ from scapy.all import *
 import sys
 
 
+TIMEOUT = 5
 targets = []
 if len(sys.argv) == 2:
 	with open( sys.argv[1] ) as f:
@@ -13,8 +14,8 @@ else:
 	while True:
 		try:
 			line = input()
-			ip,port,proto = line.split()[:3]
-			targets.append( (ip, int(port), proto) )
+			(host,port,proto) = line.split()[:3]
+			targets.append( (host, int(port), proto) )
 		except:
 			break
 
@@ -24,9 +25,9 @@ paths = TracerouteResult()
 for target in targets:
 	(host,port,proto) = target
 	if proto == 'udp':
-		paths += traceroute(host,l4=UDP(dport=port))[0]
+		paths += traceroute(host,l4=UDP(dport=port),timeout=TIMEOUT)[0]
 	elif proto == 'tcp':
-		paths += traceroute(host,dport=port)[0]
+		paths += traceroute(host,dport=port,timeout=TIMEOUT)[0]
 
 paths.graph(type="png", target=">out.png")
 print('see out.png')
